df03d00c59ffc929406ea26ec5e33b46ae7e5630
nicbn <nicolas.nattis@gmail.com>
2023-04-23 12:27:40 -0300

    Adds a small stress test

--- haz-alloc/tests/stress.rs
+++ haz-alloc/tests/stress.rs
@@ -6,4 +6,23 @@ use haz_alloc::Alloc;
 static ALLOC: Alloc = Alloc::new();
 
 #[test]
-fn stress_test() {}
+fn stress_test() {
+    let collection: Vec<Vec<usize>> = (0..256)
+        .map(|_| {
+            let mut vec = Vec::new();
+            for i in 0..4096 {
+                vec.push(i);
+            }
+            vec.reserve(10);
+            vec.reserve(20);
+            vec.shrink_to_fit();
+
+            for i in 0..4096 {
+                assert_eq!(vec[i], i);
+            }
+
+            vec
+        })
+        .collect();
+    drop(collection);
+}
